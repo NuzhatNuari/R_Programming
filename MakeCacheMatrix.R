c8ae4f4b46bc35f85430cc652a8554e1a56e2ee5
Nuzhat Nuari Khan <68327780+NuzhatNuari@users.noreply.github.com>
2021-04-16 12:43:10 +0600

    Add files via upload

--- MakeCacheMatrix.R
+++ MakeCacheMatrix.R
@@ -1,8 +1,13 @@
+##writing comments describing this functions
+
+##there is a function called makeCacheMatrix
+##makeCacheMatrix consists of set,get,setinv,getinv
+
 makeCacheMatrix = function(x = matrix) {
      inv=NULL
      set=function(y){
          x==y
-         inv=NULL
+         inv=NULL   ##setting inverse as null
      }
      get= function() {x}
      setInverse = function(inverse) {inv==inverse}
@@ -13,11 +18,11 @@ makeCacheMatrix = function(x = matrix) {
 cacheSolve = function(x, ...) {
     inv=x$getInverse()
     if(!is.null(inv)) {
-         message("getting cachedd data")
+         message("getting cachedd data") ##this is used so that we can get the previous cached matrix
          return(inv)
     }
-    mat=x$get()
-    inv=solve(mat, ...)
-    x$setInverse(inv)
+    mat=x$get()     ##this is used to get the matrix
+    inv=solve(mat, ...)  ##this will help to get the inverse
+    x$setInverse(inv)    ##this will help to set the inverse
     inv
 }
